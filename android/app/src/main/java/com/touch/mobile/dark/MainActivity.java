30e73e6212f40502e825a24b15a5434ffa532800
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-08-14 18:03:13 +0300

    Remove GLSurfaceInspector from MainActivity
    Removed GLSurfaceInspector initialization and lifecycle methods.

--- android/app/src/main/java/com/touch/mobile/dark/MainActivity.java
+++ android/app/src/main/java/com/touch/mobile/dark/MainActivity.java
@@ -1,6 +1,5 @@
 package com.touch.mobile.dark;
 
-import com.candidegardening.gpuinfo.GLSurfaceInspector;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -9,25 +8,10 @@ import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
 
-  private GLSurfaceInspector surfaceInspector = new GLSurfaceInspector();
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     RNBootSplash.init(this);
     super.onCreate(null);
-    surfaceInspector.init(this);
-  }
-
-  @Override
-  protected void onPause() {
-    super.onPause();
-    surfaceInspector.onPause();
-  }
-
-  @Override
-  protected void onResume() {
-    super.onResume();
-    surfaceInspector.onResume();
   }
 
   @Override
@@ -48,13 +32,13 @@ public class MainActivity extends ReactActivity {
     @Override
     protected ReactRootView createRootView() {
       ReactRootView reactRootView = new ReactRootView(getContext());
-      reactRootView.setIsFabric(false);  // ← تعديل: بدل BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+      reactRootView.setIsFabric(false);
       return reactRootView;
     }
 
     @Override
     protected boolean isConcurrentRootEnabled() {
-      return false;  // ← تعديل: بدل BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+      return false;
     }
   }
 }
