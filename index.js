ecbcf8c8e2712af321e666be06dc1d6cfa72c044
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-08-14 21:23:40 +0300

    Replace TestApp with main App component

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-import {AppRegistry, Text, View} from 'react-native';
+import 'react-native-gesture-handler';
+import {AppRegistry} from 'react-native';
+import App from './App';
 import {name as appName} from './app.json';
 
-const TestApp = () => (
-  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'black'}}>
-    <Text style={{color: 'red', fontSize: 30}}>TEST WORKS!</Text>
-  </View>
-);
-
-AppRegistry.registerComponent(appName, () => TestApp);
+AppRegistry.registerComponent(appName, () => App);
