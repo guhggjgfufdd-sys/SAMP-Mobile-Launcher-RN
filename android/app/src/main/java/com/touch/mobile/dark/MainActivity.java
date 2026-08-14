8bbf308b07f3396a024429389e99a79a16fa2642
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-08-14 19:46:04 +0300

    Implement onCreate method in MainActivity
    Added onCreate method to prevent crashes with React Navigation.

--- android/app/src/main/java/com/touch/mobile/dark/MainActivity.java
+++ android/app/src/main/java/com/touch/mobile/dark/MainActivity.java
@@ -1,11 +1,19 @@
 package com.touch.mobile.dark;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
 
 public class MainActivity extends ReactActivity {
 
   @Override
   protected String getMainComponentName() {
-    return "TouchMobile";
+    return "TouchMobile"; // ← تأكد من هذا الاسم
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null); // ← هذا السطر يمنع التعطل مع React Navigation
   }
 }
