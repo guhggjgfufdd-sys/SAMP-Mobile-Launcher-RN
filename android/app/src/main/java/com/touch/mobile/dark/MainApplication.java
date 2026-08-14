4e04e8995f91ccb470fdfc87e49e88ea787f1d75
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-08-14 17:17:27 +0300

    Refactor MainApplication to MainActivity structure

--- android/app/src/main/java/com/touch/mobile/dark/MainApplication.java
+++ android/app/src/main/java/com/touch/mobile/dark/MainApplication.java
@@ -1,45 +1,22 @@
 package com.touch.mobile.dark;
 
-import android.app.Application;
-import com.facebook.react.PackageList;
-import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactNativeHost;
-import com.facebook.react.ReactPackage;
-import com.facebook.soloader.SoLoader;
-import com.touch.mobile.dark.modules.GtaSetupModulePackage;
-import java.util.List;
-import androidx.multidex.MultiDexApplication;
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
-public class MainApplication extends MultiDexApplication implements ReactApplication {
-
-  private final ReactNativeHost mReactNativeHost =
-      new ReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-          return BuildConfig.DEBUG;
-        }
-
-        @Override
-        protected List<ReactPackage> getPackages() {
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          packages.add(new GtaSetupModulePackage());
-          return packages;
-        }
-
-        @Override
-        protected String getJSMainModuleName() {
-          return "index";
-        }
-      };
+public class MainActivity extends ReactActivity {
 
   @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+  protected String getMainComponentName() {
+    return "SAMP-Mobile-Launcher-RN";
   }
 
   @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, false);
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        DefaultNewArchitectureEntryPoint.getFabricEnabled());
   }
 }
