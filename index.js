b6e2a033547ea7b6c8af6bb63c9ba3d56114d94b
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-08-14 20:56:07 +0300

    Remove test comment from index.js
    Removed a comment in Arabic regarding a simple test.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import {AppRegistry, Text, View} from 'react-native';
 import {name as appName} from './app.json';
 
-// اختبار بسيط — إذا ظهر "TEST WORKS" يعني الـ Bundle يشتغل
 const TestApp = () => (
   <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'black'}}>
     <Text style={{color: 'red', fontSize: 30}}>TEST WORKS!</Text>
