e5e071d643b930c02eb2d82caf2ff6dc2e826984
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-08-14 18:27:36 +0300

    Simplify MainActivity by removing unused components
    Removed RNBootSplash initialization and MainActivityDelegate.

--- android/app/src/main/java/com/touch/mobile/dark/MainActivity.java
+++ android/app/src/main/java/com/touch/mobile/dark/MainActivity.java
@@ -1,44 +1,11 @@
 package com.touch.mobile.dark;
 
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-import android.os.Bundle;
-import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    RNBootSplash.init(this);
-    super.onCreate(null);
-  }
-
-  @Override
   protected String getMainComponentName() {
     return "TouchMobile";
-  }
-
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new MainActivityDelegate(this, getMainComponentName());
-  }
-
-  public static class MainActivityDelegate extends ReactActivityDelegate {
-    public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
-      super(activity, mainComponentName);
-    }
-
-    @Override
-    protected ReactRootView createRootView() {
-      ReactRootView reactRootView = new ReactRootView(getContext());
-      reactRootView.setIsFabric(false);
-      return reactRootView;
-    }
-
-    @Override
-    protected boolean isConcurrentRootEnabled() {
-      return false;
-    }
   }
 }
