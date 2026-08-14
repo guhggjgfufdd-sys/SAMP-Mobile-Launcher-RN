0bd1b1cf895146cf1c3a07df608ab4b303d59e5d
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-08-14 21:29:15 +0300

    Update MainActivity for new React architecture
    Refactor MainActivity to use DefaultReactActivityDelegate and enable new architecture.

--- android/app/src/main/java/com/touch/mobile/dark/MainActivity.java
+++ android/app/src/main/java/com/touch/mobile/dark/MainActivity.java
@@ -3,17 +3,27 @@ package com.touch.mobile.dark;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends ReactActivity {
 
   @Override
   protected String getMainComponentName() {
-    return "TouchMobile"; // ← تأكد من هذا الاسم
+    return "TouchMobile";
   }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null); // ← هذا السطر يمنع التعطل مع React Navigation
+    super.onCreate(null);
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        DefaultNewArchitectureEntryPoint.getFabricEnabled()
+    );
   }
 }
