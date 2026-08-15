fbaf64bef9269be06a2e55265b5e0cb07848900b
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-08-15 21:35:06 +0300

    Change launchGame parameter type to any
    Updated launchGame function to accept any data type instead of a specific ServerData interface.

--- src/modules/GtaSetupModule.ts
+++ src/modules/GtaSetupModule.ts
@@ -2,15 +2,7 @@ import { NativeModules } from 'react-native';
 
 const { GtaSetupModule } = NativeModules;
 
-interface ServerData {
-  address: string;
-  name: string;
-  playerName: string;
-}
-
 export default {
   startGame: () => GtaSetupModule.startGame(),
-  
-  // ← الدالة الجديدة
-  launchGame: (data: ServerData) => GtaSetupModule.launchGame(data),
+  launchGame: (data: any) => GtaSetupModule.launchGame(data),
 };
