122cf06f14140dfd2002524b401ebffa95ef9f69
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-08-15 21:09:05 +0300

    Implement launchGame function for Android support
    Added launchGame function to handle game launch on Android.

--- src/modules/GtaSetupModule.ts
+++ src/modules/GtaSetupModule.ts
@@ -1,3 +1,19 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
-export default NativeModules.GtaSetupModule;
+const { GtaSetupModule } = NativeModules;
+
+interface ServerData {
+  address: string;
+  name: string;
+  playerName: string;
+}
+
+export const launchGame = (serverData: ServerData): void => {
+  if (Platform.OS === 'android') {
+    GtaSetupModule.launchGame(serverData);
+  } else {
+    console.warn('launchGame is only supported on Android');
+  }
+};
+
+export default GtaSetupModule;
