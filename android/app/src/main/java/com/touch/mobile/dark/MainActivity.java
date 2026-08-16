f6ffb5e44c27f672a68ce3e6ac219d7897b4aa4c
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-08-16 19:34:20 +0300

    Add RNBootSplash initialization to MainActivity
    Initialize RNBootSplash in onCreate method.

--- android/app/src/main/java/com/touch/mobile/dark/MainActivity.java
+++ android/app/src/main/java/com/touch/mobile/dark/MainActivity.java
@@ -6,18 +6,20 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        RNBootSplash.init(this, R.style.BootTheme);
         super.onCreate(savedInstanceState);
     }
 
     @Override
     protected String getMainComponentName() {
-        return "TouchMobile"; // غيرنا ليطابق app.json
+        return "TouchMobile";
     }
 
     @Override
