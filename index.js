57a7f3a53e001075266c4cbc7cea50d067c046f9
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-08-17 00:59:49 +0300

    Reorder enableScreens call in index.js
    Ensure enableScreens is called before other imports.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import { enableScreens } from 'react-native-screens';
 
-enableScreens();  // ← لازم تكون قبل أي import ثاني
+enableScreens();
 
 import { AppRegistry } from 'react-native';
 import App from './App';
