bb1b87c4e075d1e36f3d7cd67526976613cd3a0c
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-08-16 22:31:04 +0300

    Simplify Redux integration in App component
    Replaced PersistGate with a simple Redux store setup using thunk middleware.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './src/store';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import NavigationRouter from './src/routers/navigation-router';
+
+// ====== Store بسيط جداً ======
+const rootReducer = (state = {}, action: any) => state;
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const App = () => {
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <NavigationRouter />
-      </PersistGate>
+      <NavigationRouter />
     </Provider>
   );
 };
