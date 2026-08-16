534748760264f6af53be92a7ccfba8a5fea07968
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-08-16 22:30:22 +0300

    Remove enableScreens import and invocation
    Removed the import and call to enableScreens from react-native-screens.

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 import 'react-native-gesture-handler';
-import { enableScreens } from 'react-native-screens';
-enableScreens(true);
-
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
