2e9d6a10b767bf3aa8b16b07e5d7bbe563402b9a
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-08-16 22:06:48 +0300

    Reorder import of NavigationRouter in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import NavigationRouter from './src/routers/navigation-router';
 import { store, persistor } from './src/store';
+import NavigationRouter from './src/routers/navigation-router';
 
 const App = () => {
   return (
