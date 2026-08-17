f155d688782f00da03de3962df0c7768c2a69577
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-08-17 22:35:06 +0300

    Set developer support to false
    Disable developer support in production build.

--- android/app/src/main/java/com/touch/mobile/dark/MainApplication.java
+++ android/app/src/main/java/com/touch/mobile/dark/MainApplication.java
@@ -14,7 +14,7 @@ public class MainApplication extends Application implements ReactApplication {
       new ReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
-          return BuildConfig.DEBUG;
+          return false;
         }
 
         @Override
