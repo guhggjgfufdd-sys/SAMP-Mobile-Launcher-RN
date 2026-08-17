8adb0ca1ddcda4eebb782223444cd966553f57d4
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-08-17 04:41:11 +0300

    Remove Redux and Navigation, simplify App component
    Refactor App component to use SafeAreaView and StatusBar.

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from './src/store';
-import NavigationRouter from './src/navigation/navigation-router';
+import {SafeAreaView, Text, StatusBar} from 'react-native';
 
-const App = () => {
+function App() {
   return (
-    <Provider store={store}>
-      <NavigationRouter />
-    </Provider>
+    <SafeAreaView>
+      <StatusBar barStyle="dark-content" />
+      <Text>Hello SAMP</Text>
+    </SafeAreaView>
   );
-};
+}
 
 export default App;
