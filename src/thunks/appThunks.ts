84a3f3f7148b89fb1fa28397b16c01f4f2aaddb6
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-08-18 00:23:33 +0300

    Add mock fetchPermissions function in appThunks
    Added a mock implementation for fetchPermissions to avoid missing permissionThunks file.

--- src/thunks/appThunks.ts
+++ src/thunks/appThunks.ts
@@ -4,13 +4,15 @@ import { fetchArticles } from './articleThunks';
 import { fetchDistribution } from './distributionThunks';
 import { fetchDonates } from './donateThunks';
 import { appRegisterDeviceForRemoteMessages } from './notificationThunks';
-import { fetchPermissions } from './permissionThunks';
 import { fetchInitialSettings } from './settingsThunks';
 
 // بديل وهمي لمكتبة GPU لتجاوز نقص الحزمة أثناء التجميع
 const RNGpuInfo = {
   getGlRenderer: () => 'etc',
 };
+
+// بديل وهمي لدالة الصلاحيات لتجنب عدم وجود ملف permissionThunks
+const fetchPermissions = (): AppThunk => async () => {};
 
 export const fetchInitialApp = (): AppThunk => async dispatch => {
   const glRenderer = RNGpuInfo.getGlRenderer();
