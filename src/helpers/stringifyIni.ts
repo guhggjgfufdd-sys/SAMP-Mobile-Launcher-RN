72fe10ffa80d1a2377cf932b737db773c67ccbb9
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-08-18 01:37:49 +0300

    Implement custom stringifyIni function
    Replaced the encode function from the ini library with a custom implementation to handle object serialization directly.

--- src/helpers/stringifyIni.ts
+++ src/helpers/stringifyIni.ts
@@ -1,10 +1,33 @@
-import { encode } from 'ini';
-
 interface StringifyIniOptions {
   pretty?: boolean;
 }
 
-export const stringifyIni = (data: any, options?: StringifyIniOptions) => {
-  const { pretty } = { pretty: false, ...options };
-  return encode(data, { whitespace: pretty });
+// بديل مباشر لدالة الترميز لتجاوز نقص مكتبة ini أثناء التجميع
+const encode = (data: Record<string, any>): string => {
+  let str = '';
+  for (const key in data) {
+    if (Object.prototype.hasOwnProperty.call(data, key)) {
+      const val = data[key];
+      if (typeof val === 'object' && val !== null) {
+        str += `[${key}]\n`;
+        for (const subKey in val) {
+          if (Object.prototype.hasOwnProperty.call(val, subKey)) {
+            str += `${subKey}=${val[subKey]}\n`;
+          }
+        }
+      } else {
+        str += `${key}=${val}\n`;
+      }
+    }
+  }
+  return str;
 };
+
+export const stringifyIni = (
+  obj: Record<string, any>,
+  _options?: StringifyIniOptions,
+): string => {
+  return encode(obj);
+};
+
+export default stringifyIni;
