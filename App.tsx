af829e6b6d27c62727d2900b771fb535aa4e8100
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-08-17 23:36:10 +0300

    Integrate Redux and Navigation in App component
    Refactor App component to use React Navigation and Redux.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
-import {SafeAreaView, Text, StatusBar} from 'react-native';
+import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const App = () => {
+// استيراد الشاشات من مجلد src/screens
+import { InitiationScreen } from './src/screens/InitiationScreen';
+import ModeScreen from './src/screens/ModeScreen';
+import GameScreen from './src/screens/GameScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
+
+// استيراد Redux Store
+import { store } from './src/store';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <StatusBar barStyle="dark-content" />
-      <Text style={{fontSize: 24, textAlign: 'center', marginTop: 50}}>
-        SAMP Mobile Launcher
-      </Text>
-    </SafeAreaView>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator 
+          initialRouteName="Initiation"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="Initiation" component={InitiationScreen} />
+          <Stack.Screen name="Mode" component={ModeScreen} />
+          <Stack.Screen name="Game" component={GameScreen} />
+          <Stack.Screen name="Settings" component={SettingsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
-};
-
-export default App;
+}
