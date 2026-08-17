498a6edf4158eb62021244bae98c661e22760c3a
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-08-17 07:36:16 +0300

    Add react-native.config.js for Android configuration

---
+++ react-native.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  project: {
+    android: {
+      sourceDir: './android',
+    },
+  },
+  platforms: {
+    android: {
+      npmPackageName: '@react-native-community/cli-platform-android',
+    },
+  },
+};
